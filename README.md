aab011e0f39080a631114f21462a88021281f8e3
Nick Buraglio <nick@buraglio.com>
2020-07-27 15:15:50 -0500

    Make adjustments and fix up the README

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # A simple script to grab the latest RIPE BGP route table dumps
 
-This script assums a few things:
+## This script assumes a few things:
 
-Ubuntu 18 or newer
-Understanding of BGP
-A reasonable amount of horsepower and memory to hand to the device consuming the table dump files. 
-I run this on a VM with 16G of RAM. 
+* Ubuntu 18 or newer
+* Understanding of BGP
+* A reasonable amount of horsepower and memory to hand to the device consuming the table dump files.
+* I run this on a VM with 16G of RAM.
 
+## Important notes
+* Each update file is quite large. After uncompressing some may be ~6G
+* Adjust the shell script to account for how many of the route tables you'll actually need.
+* Downloading and updating all 7 of the route tables may take quite some time
+
+## Usage
+
+  git clone https://github.com/buraglio/bgp-injector.git
+  cd bgp-injector
+  chmod +x bgp-injector-install.sh
+  ./bgp-injector-install.sh
+  vi gobgpd.conf # Make any necessary adjustments
+  sudo gobgpd -f gobgpd.conf
+  sudo gobgp mrt inject global route-tables/EU/Amsterdam/<filename>
--- bgp-injector-install.sh
+++ bgp-injector-install.sh
@@ -14,13 +14,6 @@ echo "install gobgp"
 apt update
 apt install -y gobgpd
 
-scho "create a high level folder called bgp-injector"
-
-if [[ ! -e bgp-injector/ ]]; then
-    mkdir -p bgp-injector/
-
-cd bgp-injector 
-
 echo "grab example gobgpd.conf"
 curl https://raw.githubusercontent.com/buraglio/bgp-injector/master/gobgpd.conf -o gobgpd.conf
 
@@ -40,31 +33,26 @@ if [[ ! -e route-tables/US/PaloAlto/ ]]; then
 if [[ ! -e route-tables/Africa/JoBurg/ ]]; then
     mkdir -p route-tables/Africa/JoBurg/
 
+# Uncomment the latest route table dump files for user
+
 echo "Grab latest route table from Amsterdam"
 mkdir -p route-tables/EU/Amsterdam/
-curl http://data.ris.ripe.net/rrc00/latest-bview.gz -o route-tables/EU/Amsterdam/latest-bview.gz
-#zcat updates.20200723.1835.gz | bgpdump -m - > ISP1-Europe-Amsterdam-July-2020
+curl -o - http://data.ris.ripe.net/rrc00/latest-bview.gz | gunzip > route-tables/EU/Amsterdam/latest-bview
 
 echo "Grab latest route table from London"
-curl http://data.ris.ripe.net/rrc01/latest-bview.gz -o route-tables/EU/London/latest-bview.gz
-#zcat updates.20200723.1840.gz | bgpdump -m - > ISP1-Europe-London-July-2020
+curl -o - http://data.ris.ripe.net/rrc01/latest-bview.gz | gunzip > route-tables/EU/London/latest-bview
 
 echo "Grab latest route table from Singapore"
-curl http://data.ris.ripe.net/rrc23/latest-bview.gz  -o route-tables/Asia/Singapore/latest-bview.gz
-#zcat updates.20200723.1840.gz | bgpdump -m - > ISP3-Asia-Singapore-July-2020
+curl -o - http://data.ris.ripe.net/rrc23/latest-bview.gz | gunzip > route-tables/Asia/Singapore/latest-bview
 
 echo "Grab latest route table from Sao Paulo"
-curl http://data.ris.ripe.net/rrc15/latest-bview.gz  -o route-tables/SouthAmerica/SaoPaulo/latest-bview.gz
-#zcat updates.20200723.1840.gz | bgpdump -m - > ISP4-SouthAmerica-SaoPaulo-July-2020
+curl -o - http://data.ris.ripe.net/rrc15/latest-bview.gz | gunzip > route-tables/SouthAmerica/SaoPaulo/latest-bview.gz
 
 echo "Grab latest route table from New York"
-curl http://data.ris.ripe.net/rrc11/latest-bview.gz -o route-tables/US/NewYork/latest-bview.gz
-#zcat updates.20200723.1840.gz | bgpdump -m - > ISP5-NorthAmerica-NewYork-July-2020
+curl -o - http://data.ris.ripe.net/rrc11/latest-bview.gz | gunzip > route-tables/US/NewYork/latest-bview.gz
 
 echo "Grab latest route table from Palo Alto"
-curl http://data.ris.ripe.net/rrc14/latest-bview.gz -o route-tables/US/PaloAlto/latest-bview.gz
-#zcat updates.20200723.1845.gz | bgpdump -m - > ISP6-NorthAmerica-PaloAlto-July-2020
+curl -o - http://data.ris.ripe.net/rrc14/latest-bview.gz | gunzip > route-tables/US/PaloAlto/latest-bview.gz
 
 echo "Grab latest route table from Johannasburg"
-curl http://data.ris.ripe.net/rrc19/latest-bview.gz -o route-tables/Africa/JoBurg/latest-bview.gz
-#zcat updates.20200723.1845.gz | bgpdump -m - > ISP7-Africa-JoBerg-July-2020
+curl -o - http://data.ris.ripe.net/rrc19/latest-bview.gz | gunzip > route-tables/Africa/JoBurg/latest-bview.gz
