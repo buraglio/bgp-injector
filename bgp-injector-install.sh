6b1d40f75524e693f05622c9065e932b7ec3c0e4
Nick Buraglio <nick@buraglio.com>
2020-07-27 16:34:19 -0500

    Add better comments and echo statements

--- bgp-injector-install.sh
+++ bgp-injector-install.sh
@@ -2,22 +2,42 @@
 # A simple script to grab the latest RIPE BGP route table dumps
 # created by buraglio@forwardingplane.net
 # https://www.forwardingplane.net
-
+echo" "
+echo" "
+echo "##################################################################"
 echo "This script will install goBGP and create the directory"
 echo "structure for using it as a route server for MRT files"
 echo "It assumed ubuntu 18 or newer"
 echo "please note that consuming and re-advertising a full table feed"
 echo "from RIPE will require a significant amount of memory (16G is recommended)"
-
-
-echo "install gobgp"
+echo "##################################################################"
+echo" "
+echo" "
+echo" "
+echo "################################"
+echo "install gobgp, curl"
+echo "################################"
+echo" "
+echo" "
+echo" "
 sudo apt update
 sudo apt install -y gobgpd curl
-
+echo" "
+echo" "
+echo "################################"
 echo "grab example gobgpd.conf"
+echo "################################"
+echo" "
+echo" "
 curl https://raw.githubusercontent.com/buraglio/bgp-injector/master/gobgpd.conf -o gobgpd.conf
 
+echo "################################"
 echo "Create directories for each route table if they don't exist"
+echo "################################"
+echo" "
+echo" "
+echo" "
+echo" "
 
 createDir(){
 	if [ ! -d route-tables/EU/Amsterdam/ ]
@@ -77,25 +97,38 @@ createDir(){
 }
 
 # Uncomment the latest route table dump files for user
-
+echo "##################################################################"
 echo "Grab latest route table from Amsterdam"
+echo "##################################################################"
 mkdir -p route-tables/EU/Amsterdam/
 curl -o - http://data.ris.ripe.net/rrc00/latest-bview.gz | gunzip > route-tables/EU/Amsterdam/latest-bview
 
+echo "##################################################################"
 echo "Grab latest route table from London"
 curl -o - http://data.ris.ripe.net/rrc01/latest-bview.gz | gunzip > route-tables/EU/London/latest-bview
+echo "##################################################################"
 
+echo "##################################################################"
 echo "Grab latest route table from Singapore"
+echo "##################################################################"
 curl -o - http://data.ris.ripe.net/rrc23/latest-bview.gz | gunzip > route-tables/Asia/Singapore/latest-bview
 
+echo "##################################################################"
 echo "Grab latest route table from Sao Paulo"
+echo "##################################################################"
 curl -o - http://data.ris.ripe.net/rrc15/latest-bview.gz | gunzip > route-tables/SouthAmerica/SaoPaulo/latest-bview.gz
 
+echo "##################################################################"
 echo "Grab latest route table from New York"
+echo "##################################################################"
 curl -o - http://data.ris.ripe.net/rrc11/latest-bview.gz | gunzip > route-tables/US/NewYork/latest-bview.gz
 
+echo "##################################################################"
 echo "Grab latest route table from Palo Alto"
+echo "##################################################################"
 curl -o - http://data.ris.ripe.net/rrc14/latest-bview.gz | gunzip > route-tables/US/PaloAlto/latest-bview.gz
 
+echo "##################################################################"
 echo "Grab latest route table from Johannasburg"
+echo "##################################################################"
 curl -o - http://data.ris.ripe.net/rrc19/latest-bview.gz | gunzip > route-tables/Africa/JoBurg/latest-bview.gz
